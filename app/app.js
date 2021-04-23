943ceb762fe4e33506cbbf24b88928af546eb6e0
root <root@li72-133.members.linode.com>
2021-04-23 13:43:47 +0800

    fix

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@
 // TODO: Put go into a config.js
 // But how to include a file from local?
 
-var GETH_HOSTNAME = "localhost";	// put your IP address!
-var APP_HOSTNAME = "See package.json --> scripts --> start: Change 'localhost'!!!";
+var GETH_HOSTNAME = "74.207.230.133";	// put your IP address!
+var APP_HOSTNAME = "gtc2.hayoou.com";
 
 var GETH_RPCPORT = 8545; 		// for geth --rpcport GETH_RPCPORT
-var APP_PORT = "See package.json --> scripts --> start: Perhaps change '8000'";
+var APP_PORT = "8000";
 
 // this is creating the corrected geth command
 var WL = window.location;
